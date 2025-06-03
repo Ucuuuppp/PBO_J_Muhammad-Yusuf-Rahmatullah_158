b8f4d71cd8908fd09993f373f7c967ba133815c1
Ucuuuppp <yrahmatullah111@gmail.com>
2025-06-03 21:22:06 +0700

    update module-info.java to include transitive javafx.base requirement

--- TUGAS/modul6/src/main/java/module-info.java
+++ TUGAS/modul6/src/main/java/module-info.java
@@ -1,11 +1,11 @@
 module com.yusuf {
     requires javafx.controls;
     requires javafx.fxml;
+    requires transitive javafx.base;
 
     opens com.yusuf to javafx.fxml;
     opens com.yusuf.data to javafx.base;
     opens com.yusuf.users to javafx.base;
-
 
     exports com.yusuf;
 }
